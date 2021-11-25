400fe997c1ff8cdb0066b9f4127b71711b8c1f64
lihsin <lihsin0427@gmail.com>
2021-11-26 00:44:43 +0800

    Move helpers to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < ActionController::Base
   include UsersHelper
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  helper_method :user_signed_in?, :current_user
 
+  private
   def record_not_found  
     render file: 'public/404.html', layout: false, status: 404
   end
+
+  def user_signed_in?
+    session[:welcome].present?
+  end
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:welcome])
+  end
+
 end
