a98f81616e84214f63122c5976f8269e01939030
lihsin <lihsin0427@gmail.com>
2021-12-06 16:33:28 +0800

    code_generator

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,14 @@ class Order < ApplicationRecord
 
   private 
   def serial_generator
-    serial = "NFT#{Time.current.strftime("%Y%m%d")}#{id.to_s.rjust(7, '0')}"
+    serial = "NFT#{Time.current.strftime("%Y%m%d")}#{code_generator(7)}"
     update(serial: serial)
+  end
+
+  def code_generator(n = 7)
+    all_chars = [*'A'..'Z', *'0'..'9']
+    confused_chars = ['X', 'O', '0', 'B', 'P', 'M', 'N', 'D', 'T']
+
+    (all_chars - confused_chars).sample(n).join
   end
 end
