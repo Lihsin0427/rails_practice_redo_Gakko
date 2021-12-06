545ef3c2c9fba53bc0d4528570358b75842a9ea6
lihsin <lihsin0427@gmail.com>
2021-12-06 13:48:48 +0800

    Install Stimulus.js,改寫js

---
+++ app/javascript/controllers/favorite_controller.js
@@ -0,0 +1,39 @@
+// Visit The Stimulus Handbook for more details 
+// https://stimulusjs.org/handbook/introduction
+// 
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+import httpClient from "lib/http/client"
+
+export default class extends Controller {
+  static targets = ["btn"]
+  static values = {
+    liked: Boolean,
+    courseId: Number,
+  }
+
+  connect(){
+    if (this.likedValue){
+    this.btnTarget.classList.add("favorited")
+    }
+  }
+
+  like() {
+    const courseID = this.courseIdValue
+    // const likeBtn = this.btnTarget
+
+    httpClient.post(`/api/v1/courses/${courseID}/like`).then(({ data }) => {
+      // console.log(data)
+      if (data.result === "like"){
+        this.btnTarget.classList.add("favorited")
+      } else {
+        this.btnTarget.classList.remove("favorited")
+      }
+    })
+  }
+}
--- app/javascript/script/course.js
+++ app/javascript/script/course.js
@@ -1,22 +1,22 @@
-import httpClient from "lib/http/client"
+// import httpClient from "lib/http/client"
 
-document.addEventListener("turbolinks:load", () => {
+// document.addEventListener("turbolinks:load", () => {
 
-  const likeBtn = document.querySelector("#like-btn")
+//   const likeBtn = document.querySelector("#like-btn")
 
-  if (likeBtn) {
-    likeBtn.addEventListener("click", () => {
-      const courseID = likeBtn.dataset.id
+//   if (likeBtn) {
+//     likeBtn.addEventListener("click", () => {
+//       const courseID = likeBtn.dataset.id
 
-      httpClient.post(`/api/v1/courses/${courseID}/like`).then(({ data }) => {
-        console.log(data)
-        if (data.result === "like"){
-          likeBtn.classList.add("favorited")
-        } else {
-          likeBtn.classList.remove("favorited")
-        }
-      })
-    })
-  }
-})
+//       httpClient.post(`/api/v1/courses/${courseID}/like`).then(({ data }) => {
+//         console.log(data)
+//         if (data.result === "like"){
+//           likeBtn.classList.add("favorited")
+//         } else {
+//           likeBtn.classList.remove("favorited")
+//         }
+//       })
+//     })
+//   }
+// })
 
